f884422f45898ce45803ffd3636a25f5ead8fc31
Eric W. Greene <eric@ericwgreene.com>
2016-12-31 17:31:31 -0500

    Minor fixes to photo tiles.

--- sql/photo_tiles.sql
+++ sql/photo_tiles.sql
@@ -18,13 +18,13 @@
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."photo_tiles";
 CREATE TABLE "public"."photo_tiles" (
-	"id" int8 NOT NULL DEFAULT nextval('photo_tiles_id_seq'::regclass),
-	"caption" varchar(30) NOT NULL COLLATE "default",
-	"normal_url" varchar(200) NOT NULL COLLATE "default",
-	"hover_url" varchar(200) NOT NULL COLLATE "default"
+	"id" bigserial NOT NULL,
+	"caption" varchar(30) NOT NULL,
+	"normal_url" varchar(200) NOT NULL,
+	"hover_url" varchar(200) NOT NULL
 )
 WITH (OIDS=FALSE);
-ALTER TABLE "public"."photo_tiles" OWNER TO "ericwgreene";
+ALTER TABLE "public"."photo_tiles" OWNER TO "photo_web_site";
 
 -- ----------------------------
 --  Records of photo_tiles
@@ -34,12 +34,11 @@ INSERT INTO "public"."photo_tiles" VALUES ('1', 'Hawaii', 'images/hawaii_bw.jpg'
 INSERT INTO "public"."photo_tiles" VALUES ('2', 'New York', 'images/newyork_bw.jpg', 'images/newyork_color.jpg');
 INSERT INTO "public"."photo_tiles" VALUES ('3', 'California', 'images/goldengate_bw.jpg', 'images/goldengate_color.jpg');
 INSERT INTO "public"."photo_tiles" VALUES ('4', 'Canada', 'images/canada_bw.jpg', 'images/canada_color.jpg');
-INSERT INTO "public"."photo_tiles" VALUES ('5', 'Sarah', 'images/hawaii_bw.jpg', 'images/sarah_color.jpg');
-INSERT INTO "public"."photo_tiles" VALUES ('6', 'Nature', 'images/hawaii_bw.jpg', 'images/sarah_color.jpg');
+INSERT INTO "public"."photo_tiles" VALUES ('5', 'Sarah', 'images/sarah_bw.jpg', 'images/sarah_color.jpg');
+INSERT INTO "public"."photo_tiles" VALUES ('6', 'Nature', 'images/hawaii_bw.jpg', 'images/nature_color.jpg');
 INSERT INTO "public"."photo_tiles" VALUES ('7', 'Animals', 'images/animals_bw.jpg', 'images/animals_color.jpg');
 INSERT INTO "public"."photo_tiles" VALUES ('8', 'Hawaii', 'images/hawaii_bw.jpg', 'images/hawaii_color.jpg');
-INSERT INTO "public"."photo_tiles" VALUES ('9', 'Food',
-'images/cappuccino_bw.jpg', 'images/cappuccino_color.jpg');
+INSERT INTO "public"."photo_tiles" VALUES ('9', 'Food','images/cappuccino_bw.jpg', 'images/cappuccino_color.jpg');
 COMMIT;
 
 -- ----------------------------
